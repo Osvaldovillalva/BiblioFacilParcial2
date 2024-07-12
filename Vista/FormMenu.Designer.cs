ea2ab003ce32aeef223984bd3700f50c9e24f68e
javier <javierzaurio@gmail.com>
2024-07-12 08:16:49 -0300

    mas cambios

--- Vista/FormMenu.Designer.cs
+++ Vista/FormMenu.Designer.cs
@@ -49,7 +49,7 @@
             buttonSocios.FlatStyle = FlatStyle.Flat;
             buttonSocios.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
             buttonSocios.ForeColor = SystemColors.ButtonFace;
-            buttonSocios.Location = new Point(0, 56);
+            buttonSocios.Location = new Point(12, 67);
             buttonSocios.Name = "buttonSocios";
             buttonSocios.Size = new Size(170, 50);
             buttonSocios.TabIndex = 1;
@@ -64,7 +64,7 @@
             buttonLibros.FlatStyle = FlatStyle.Flat;
             buttonLibros.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
             buttonLibros.ForeColor = SystemColors.ButtonFace;
-            buttonLibros.Location = new Point(0, 133);
+            buttonLibros.Location = new Point(12, 133);
             buttonLibros.Name = "buttonLibros";
             buttonLibros.Size = new Size(170, 50);
             buttonLibros.TabIndex = 0;
@@ -78,7 +78,7 @@
             button1.FlatStyle = FlatStyle.Flat;
             button1.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
             button1.ForeColor = SystemColors.ButtonFace;
-            button1.Location = new Point(12, 323);
+            button1.Location = new Point(12, 263);
             button1.Name = "button1";
             button1.Size = new Size(170, 59);
             button1.TabIndex = 2;
@@ -93,7 +93,7 @@
             buttonPrestamos.FlatStyle = FlatStyle.Flat;
             buttonPrestamos.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
             buttonPrestamos.ForeColor = SystemColors.ButtonFace;
-            buttonPrestamos.Location = new Point(12, 225);
+            buttonPrestamos.Location = new Point(12, 189);
             buttonPrestamos.Name = "buttonPrestamos";
             buttonPrestamos.Size = new Size(170, 50);
             buttonPrestamos.TabIndex = 2;
@@ -106,9 +106,9 @@
             button2.FlatStyle = FlatStyle.Popup;
             button2.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
             button2.ForeColor = SystemColors.ButtonFace;
-            button2.Location = new Point(272, 225);
+            button2.Location = new Point(12, 363);
             button2.Name = "button2";
-            button2.Size = new Size(194, 71);
+            button2.Size = new Size(170, 50);
             button2.TabIndex = 3;
             button2.Text = "Registros";
             button2.UseVisualStyleBackColor = true;
@@ -119,9 +119,9 @@
             buttonCuota.FlatStyle = FlatStyle.Popup;
             buttonCuota.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
             buttonCuota.ForeColor = SystemColors.ButtonFace;
-            buttonCuota.Location = new Point(283, 58);
+            buttonCuota.Location = new Point(228, 67);
             buttonCuota.Name = "buttonCuota";
-            buttonCuota.Size = new Size(197, 69);
+            buttonCuota.Size = new Size(170, 60);
             buttonCuota.TabIndex = 2;
             buttonCuota.Text = "Asignar valor Cuota";
             buttonCuota.UseVisualStyleBackColor = true;
@@ -136,7 +136,7 @@
             buttonSalir.ForeColor = SystemColors.ButtonFace;
             buttonSalir.Image = (Image)resources.GetObject("buttonSalir.Image");
             buttonSalir.ImageAlign = ContentAlignment.MiddleLeft;
-            buttonSalir.Location = new Point(568, 449);
+            buttonSalir.Location = new Point(573, 440);
             buttonSalir.Name = "buttonSalir";
             buttonSalir.Size = new Size(350, 50);
             buttonSalir.TabIndex = 1;
@@ -149,9 +149,9 @@
             buttonCobroCuotas.FlatStyle = FlatStyle.Popup;
             buttonCobroCuotas.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
             buttonCobroCuotas.ForeColor = SystemColors.ButtonFace;
-            buttonCobroCuotas.Location = new Point(283, 133);
+            buttonCobroCuotas.Location = new Point(228, 133);
             buttonCobroCuotas.Name = "buttonCobroCuotas";
-            buttonCobroCuotas.Size = new Size(194, 71);
+            buttonCobroCuotas.Size = new Size(170, 50);
             buttonCobroCuotas.TabIndex = 3;
             buttonCobroCuotas.Text = "Cobrar Cuota";
             buttonCobroCuotas.UseVisualStyleBackColor = true;
